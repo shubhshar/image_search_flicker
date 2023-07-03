4c9bfeeb82b8e21c71639d9ce608e7f77a94e072
Shubham Sharma <shubhr0813@gmail.com>
2023-07-03 18:45:07 +0530

    changes in api key and url.

--- src/services/api.js
+++ src/services/api.js
@@ -4,15 +4,12 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const fetchData = async (page, perpage, searchq) => {
   try {
-    const method = searchq ? "flickr.photos.search" : "flickr.photos.getRecent";
-    const response = await axios.get(
-      ` https://www.flickr.com/services/rest/?method=${method}&api_key=${API_KEY}&safe_search=1&per_page=${perpage}&page=${page}&format=json&nojsoncallback=1`,
-      {
-        params: {
-          text: searchq,
-        },
-      }
-    );
+    const searchUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${searchq}&safe_search=1&per_page=${perpage}&page=${page}&format=json&nojsoncallback=1`;
+
+    const getUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${API_KEY}&safe_search=1&per_page=${perpage}&page=${page}&format=json&nojsoncallback=1`;
+
+    const url = searchq ? searchUrl : getUrl;
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.log("Error: ", error);
